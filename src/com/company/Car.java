b52155a87ddde016ba6dda27e7b8e77bbcfb5704
13 <baktyguljumagazyeva@gmail.com>
2022-09-01 20:44:57 +0600

    TT 20

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,2 +1,12 @@
-package com.company;public class Car {
-}
+package com.company;
+
+    public class Car implements AutoCloseable {
+        public void close(){
+            System.out.println("Машина жабылып жатат");
+        }
+        public static void drive(){
+            System.out.println("Mашина журуп жатат");
+        }
+    }
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException{
+
+        try (Car car = new Car()){
+
+            Car.drive();
+            throw new ArrayIndexOutOfBoundsException();
+        }catch (ArrayIndexOutOfBoundsException e){
+
+        }
     }
 }
